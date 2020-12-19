d1fce0c6ba772b878c5e2b8245c087282b8e7e43
Baptiste <baptiste63.foucras@gmail.com>
2020-12-19 15:25:52 +0100

    Baptiste ajout struct Adherent et modif id dans txt

--- projetS1.h
+++ projetS1.h
@@ -2,3 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+typedef struct
+{
+	int idAdherent;
+	char civilite[3];
+	char nom[20];
+	char prenom[20];
+	int jour_inscrip;
+	int mois_inscrip;
+	int annee_inscrip;
+}Adherent;
